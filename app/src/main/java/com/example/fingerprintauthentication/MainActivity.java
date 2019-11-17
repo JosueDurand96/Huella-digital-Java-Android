935140fd5e3f66fe61b2607e7fff49542edecc60
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 16:37:14 -0500

    main

--- app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
+++ app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.concurrent.Executor;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Executor executor = new Executor()
     }
 }
