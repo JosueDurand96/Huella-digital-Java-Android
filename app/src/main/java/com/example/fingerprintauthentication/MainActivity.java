64014bac34f1a125d83a050414a4673811d56556
JosueDurand96 <josue.durand8@gmail.com>
2019-11-17 16:40:04 -0500

    main

--- app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
+++ app/src/main/java/com/example/fingerprintauthentication/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.fingerprintauthentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.biometrics.BiometricManager;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
 
 import java.util.concurrent.Executor;
-
+import java.util.concurrent.Executors;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Executor executor = new Executor()
+        Executor executor =  Executor.newSingleThreadExecutor();
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt.Builder(this)
+
     }
 }
